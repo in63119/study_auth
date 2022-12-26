dc8f4f95b153fee74f2a955c9457c5cb3accd295
in63119 <65399118+in63119@users.noreply.github.com>
2022-12-27 05:59:10 +0900

    Update callback.js

--- oauth/server-oauth/controller/callback.js
+++ oauth/server-oauth/controller/callback.js
@@ -6,7 +6,7 @@ const axios = require('axios');
 
 module.exports = (req, res) => {
   // req의 body로 authorization code가 들어옵니다. console.log를 통해 서버의 터미널창에서 확인해보세요!
-  console.log(req.body);
+  console.log(req);
 
   // TODO : 이제 authorization code를 이용해 access token을 발급받기 위한 post 요청을 보냅니다. 다음 링크를 참고하세요.
   // https://docs.github.com/en/free-pro-team@latest/developers/apps/identifying-and-authorizing-users-for-github-apps#2-users-are-redirected-back-to-your-site-by-github
